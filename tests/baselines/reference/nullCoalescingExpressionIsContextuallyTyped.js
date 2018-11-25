e42af6fa982dbf054b7a7a396089c91a7af6baa1
Ryan Petrich <rpetrich@gmail.com>
2018-11-25 14:38:36 -0500

    Add tests for null coalescing operator

---
+++ tests/baselines/reference/nullCoalescingExpressionIsContextuallyTyped.js
@@ -0,0 +1,15 @@
+//// [nullCoalescingExpressionIsContextuallyTyped.ts]
+// The ?? operator permits the operands to be of any type.
+// If the ?? expression is contextually typed, the operands are contextually typed by the
+// same type and the result is of the best common type of the contextual type and the two
+// operand types.
+
+var r: { a: string } = { a: '', b: 123 } ?? { a: '', b: true };
+
+//// [nullCoalescingExpressionIsContextuallyTyped.js]
+// The ?? operator permits the operands to be of any type.
+// If the ?? expression is contextually typed, the operands are contextually typed by the
+// same type and the result is of the best common type of the contextual type and the two
+// operand types.
+var _a;
+var r = (_a = { a: '', b: 123 }) != null ? _a : { a: '', b: true };
---
+++ tests/baselines/reference/nullCoalescingExpressionIsNotContextuallyTyped.js
@@ -0,0 +1,20 @@
+//// [nullCoalescingExpressionIsNotContextuallyTyped.ts]
+// The ?? operator permits the operands to be of any type.
+// If the ?? expression is not contextually typed, the right operand is contextually typed
+// by the type of the left operand and the result is of the best common type of the two
+// operand types.
+
+
+var a: (a: string) => string;
+
+// bug 786110
+var r = a ?? ((a) => a.toLowerCase());
+
+//// [nullCoalescingExpressionIsNotContextuallyTyped.js]
+// The ?? operator permits the operands to be of any type.
+// If the ?? expression is not contextually typed, the right operand is contextually typed
+// by the type of the left operand and the result is of the best common type of the two
+// operand types.
+var a;
+// bug 786110
+var r = a != null ? a : (function (a) { return a.toLowerCase(); });
